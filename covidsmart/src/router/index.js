cfa1d1c7bd1bfef121fe6c09b5c0086461880024
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-24 00:09:42 -0700

    asdf

--- covidsmart/src/router/index.js
+++ covidsmart/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
 import login from '@/components/SignIn'
+import dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
 
@@ -17,6 +18,12 @@ export default new Router({
       path: '/',
       name: 'login',
       component: login
+    },
+
+    {
+      path: '/',
+      name: 'dashboard',
+      component: dashboard
     },
    
   ]
