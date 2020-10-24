d568e44f7f0920ac96c6cfe497540cf24fe662b5
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-24 00:28:28 -0700

    asdf

--- covidsmart/src/components/home.vue
+++ covidsmart/src/components/home.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
 
+      /* eslint-disable */
+
     <router-link to="/login"  href="/login" style="text-decoration: none; color: #898989; margin-left: 90%;">Login/Signup</router-link>
 
     <div style="margin-left: 15%">
