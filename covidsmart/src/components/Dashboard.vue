15ec843ee95172b410bde18f32f9ce7d7d809363
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-10-24 01:16:19 -0700

    migrated dashboard

--- covidsmart/src/components/Dashboard.vue
+++ covidsmart/src/components/Dashboard.vue
@@ -1,18 +1,166 @@
-
 <template>
+
     <div>
-        <h1 style="color: white">Hello</h1>
         <Sidebar />
+
+        <div id="container-body-main" >
+            <h1 class="my-4">Dashboard
+      </h1>
+            <hr>
+
+
+          <div class="row">
+
+        <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Encounters(Today)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">{{ today }}</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Hot Spots</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"> 890 Marcello Avn.</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Large Groups</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+            <div class="card shadow mb-4" style="margin-right: 30px">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">More Stats(out of a 100)</h6>
+                </div>
+                <div class="card-body">
+                  <h4 class="small font-weight-bold">Encounters <span class="float-right">20%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Amount of Large Groups <span class="float-right">20%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">You Should get Tested <span class="float-right">20%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+
+        </div>
+
+
     </div>
 </template>
 
 <script>
-import Sidebar from "@/components/Sidebar";
+import Sidebar from '@/components/Sidebar'
+var Chart = require('@/vendor/chartJS/Chart.js')
+import init from "@/components/init";
+import firebase from "firebase";
+
 export default {
-    name: 'dashboard',
+    name: 'Dashboard',
+    components: {
+        Sidebar,
+    },
+    data() {
+        return {
+          encountersToday: [],
+          today: null,
+          data: [],
+
+        }
+    },
+
+    beforeMount() {
+      this.getTodayEncounters()
+    },
+
+    methods: {
+      getTodayEncounters() {
+        
+
+        firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            var name = user.displayName;
+            var email = user.email;
+            
+            
+
+        firebase.firestore().collection('Users').doc(email).get().then(snapshot => {
+              
+                let data = snapshot.data();
+                let today = data['totalencounters']
+                this.today = today
+                this.encountersToday.push(data)
+              
+
+            })
+
+        firebase.firestore().collection('Logs').where('email', '==', email).get().then(snapshot => {
+          snapshot.forEach(doc => {
+                let data = doc.data();
+                this.today = today
+                this.data.push(data)
+
+          })
+              
+                
+              
+
+            })
+      }
+      })
+        
+      }
+    },
+
+    
 }
 </script>
 
 <style>
+
+#container-body-main{
+    margin-left: 253px; 
+    margin-top: 3%
+}
 
 </style>
--- covidsmart/src/components/Sidebar.vue
+++ covidsmart/src/components/Sidebar.vue
@@ -1,24 +1,452 @@
 <template>
-      <div>
-    <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
-    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-      </div>
-    </b-sidebar>
-  </div>
+    <div class="wrapper" style="position: fixed">
+        <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg" style="position: fixed">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+    	<div class="sidebar-wrapper">
+            <div class="logo">
+                COVID NETWORK
+            </div>
+
+            <ul class="nav">
+                <li >
+                    <a href="dashboard.html">
+                        <i class=""></i>
+                        <center>
+                            <router-link :to="{name: 'Dashboard'}" style="color: white">
+                                <p>Dashboard</p>
+                            </router-link>
+                        </center>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class=""></i>
+                        <router-link :to="{name: 'Maps'}" style="color: white">
+                            <p>Maps </p>
+                        </router-link>
+                        
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="table.html">
+                        <i class="pe-7s-note2"></i>
+                        <router-link :to="{name: 'News'}" style="color: white">
+                            <p style="text-align: center">Health News</p>
+                        </router-link>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class=""></i>
+                        <router-link :to="{name: 'Logs'}" style="color: white">
+                            <p>Armed Log</p>
+                        </router-link>
+                        
+                    </a>
+                </li>
+                <li>
+                    <a href="typography.html">
+                        <i class="pe-7s-news-paper"></i>
+                        <router-link :to="{name: 'Stats'}" style="color: white">
+                                <p style="text-align: center">COVID Stats</p>
+                            </router-link>
+                        
+                    </a>
+                </li>
+                
+				<li class="active-pro">
+                    <a href="upgrade.html">
+                        <i class="pe-7s-rocket"></i>
+                        <center></center>
+                        <p>Logout</p>
+                    </a>
+                </li>
+            </ul>
+    	</div>
+    </div>
+    </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 export default {
+
     name: 'Sidebar',
+    
+    data() {
+        return {
+
+        }
+    }
+    
 }
 </script>
 
 <style>
+.sidebar .nav a,
+.table > tbody > tr .td-actions .btn {
+    -webkit-transition: all 150ms ease-in;
+    -moz-transition: all 150ms ease-in;
+    -o-transition: all 150ms ease-in;
+    -ms-transition: all 150ms ease-in;
+    transition: all 150ms ease-in;
+}
+
+    
+.sidebar {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: auto;
+    left: 0;
+    width: 260px;
+    display: block;
+    z-index: 1;
+    color: #fff;
+    font-weight: 200;
+    background-size: cover;
+    background-position: center center;
+}
+
+.sidebar .sidebar-wrapper {
+    position: relative;
+    max-height: calc(100vh - 75px);
+    min-height: 100%;
+    overflow: auto;
+    width: 260px;
+    z-index: 4;
+    padding-bottom: 100px;
+}
+
+.sidebar .sidebar-background {
+    position: absolute;
+    z-index: 1;
+    height: 100%;
+    width: 100%;
+    display: block;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    background-position: center center;
+}
+
+.sidebar .logo {
+    padding: 10px 15px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+.sidebar .logo p {
+    float: left;
+    font-size: 20px;
+    margin: 10px 10px;
+    color: #FFFFFF;
+    line-height: 20px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.sidebar .logo .simple-text {
+    text-transform: uppercase;
+    padding: 5px 0px;
+    display: block;
+    font-size: 18px;
+    color: #FFFFFF;
+    text-align: center;
+    font-weight: 400;
+    line-height: 30px;
+}
+
+.sidebar .logo-tim {
+    border-radius: 50%;
+    border: 1px solid #333;
+    display: block;
+    height: 61px;
+    width: 61px;
+    float: left;
+    overflow: hidden;
+}
+
+.sidebar .logo-tim img {
+    width: 60px;
+    height: 60px;
+}
+
+.sidebar .nav {
+    margin-top: 20px;
+    float: none;
+}
+
+.sidebar .nav .open > a,
+.sidebar .nav li.dropdown .dropdown-menu li:hover > a,
+.sidebar .nav li:hover > a {
+    background-color: rgba(255, 255, 255, 0.13);
+    opacity: 1;
+}
+
+.sidebar .nav li > a {
+    color: #FFFFFF;
+    margin: 5px 15px;
+    opacity: .86;
+    border-radius: 4px;
+    display: block;
+}
+
+.sidebar .nav li.active > a {
+    color: #FFFFFF;
+    opacity: 1;
+    background: rgba(255, 255, 255, 0.23);
+}
+
+.sidebar .nav li.separator {
+    margin: 15px 0;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+.sidebar .nav li.separator + li {
+    margin-top: 31px;
+}
+
+.sidebar .nav p {
+    margin: 0;
+    line-height: 30px;
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-left: 45px;
+}
+
+.sidebar .nav i {
+    font-size: 28px;
+    float: left;
+    margin-right: 15px;
+    line-height: 30px;
+    width: 30px;
+    text-align: center;
+}
+
+.sidebar .nav .caret {
+    margin-top: 13px;
+    position: absolute;
+    right: 30px;
+}
+
+.sidebar .logo {
+    padding: 10px 15px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+.sidebar .logo p {
+    float: left;
+    font-size: 20px;
+    margin: 10px 10px;
+    color: #FFFFFF;
+    line-height: 20px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.sidebar .logo .simple-text {
+    text-transform: uppercase;
+    padding: 5px 0px;
+    display: block;
+    font-size: 18px;
+    color: #FFFFFF;
+    text-align: center;
+    font-weight: 400;
+    line-height: 30px;
+}
+
+.sidebar .logo-tim {
+    border-radius: 50%;
+    border: 1px solid #333;
+    display: block;
+    height: 61px;
+    width: 61px;
+    float: left;
+    overflow: hidden;
+}
+
+.sidebar .logo-tim img {
+    width: 60px;
+    height: 60px;
+}
+
+.sidebar:after, .sidebar:before {
+    display: block;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 2;
+}
+
+.sidebar:before {
+    opacity: .33;
+    background: #000000;
+}
+
+.sidebar:after {
+    background: #787878;
+    background: -moz-linear-gradient(top, #787878 0%, #343434 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #787878), color-stop(100%, #343434));
+    background: -webkit-linear-gradient(top, #787878 0%, #343434 100%);
+    background: -o-linear-gradient(top, #787878 0%, #343434 100%);
+    background: -ms-linear-gradient(top, #787878 0%, #343434 100%);
+    background: linear-gradient(to bottom, #787878 0%, #343434 100%);
+    background-size: 150% 150%;
+    z-index: 3;
+    opacity: 1;
+}
+
+.sidebar[data-image]:after, .sidebar.has-image:after {
+    opacity: .77;
+}
+
+.sidebar[data-color="blue"]:after {
+    background: #1F77D0;
+    background: -moz-linear-gradient(top, #1F77D0 0%, #533ce1 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #1F77D0), color-stop(100%, #533ce1));
+    background: -webkit-linear-gradient(top, #1F77D0 0%, #533ce1 100%);
+    background: -o-linear-gradient(top, #1F77D0 0%, #533ce1 100%);
+    background: -ms-linear-gradient(top, #1F77D0 0%, #533ce1 100%);
+    background: linear-gradient(to bottom, #1F77D0 0%, #533ce1 100%);
+    background-size: 150% 150%;
+}
+
+.sidebar[data-color="azure"]:after {
+    background: #1DC7EA;
+    background: -moz-linear-gradient(top, #1DC7EA 0%, #4091ff 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #1DC7EA), color-stop(100%, #4091ff));
+    background: -webkit-linear-gradient(top, #1DC7EA 0%, #4091ff 100%);
+    background: -o-linear-gradient(top, #1DC7EA 0%, #4091ff 100%);
+    background: -ms-linear-gradient(top, #1DC7EA 0%, #4091ff 100%);
+    background: linear-gradient(to bottom, #1DC7EA 0%, #4091ff 100%);
+    background-size: 150% 150%;
+}
+
+.sidebar[data-color="green"]:after {
+    background: #87CB16;
+    background: -moz-linear-gradient(top, #87CB16 0%, #6dc030 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #87CB16), color-stop(100%, #6dc030));
+    background: -webkit-linear-gradient(top, #87CB16 0%, #6dc030 100%);
+    background: -o-linear-gradient(top, #87CB16 0%, #6dc030 100%);
+    background: -ms-linear-gradient(top, #87CB16 0%, #6dc030 100%);
+    background: linear-gradient(to bottom, #87CB16 0%, #6dc030 100%);
+    background-size: 150% 150%;
+}
+
+.sidebar[data-color="orange"]:after {
+    background: #FFA534;
+    background: -moz-linear-gradient(top, #FFA534 0%, #ff5221 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #FFA534), color-stop(100%, #ff5221));
+    background: -webkit-linear-gradient(top, #FFA534 0%, #ff5221 100%);
+    background: -o-linear-gradient(top, #FF7070 0%, #ff5221 100%);
+    background: -ms-linear-gradient(top, #FF7070 0%, #ff5221 100%);
+    background: linear-gradient(to bottom, #FF7070 0%, #ff5221 100%);
+    background-size: 150% 150%;
+}
+
+.sidebar[data-color="red"]:after {
+    background: #FF7070;
+    background: -moz-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #FB404B), color-stop(100%, #FF7070));
+    background: -webkit-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: -o-linear-gradient(top, #FB40#FF7070 0%, #FF7070 100%);
+    background: -ms-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: linear-gradient(to bottom, #FF7070 0%, #FF7070 100%);
+    background-size: 150% 150%;
+}
+
+.sidebar[data-color="purple"]:after {
+    background: #FF7070;
+    background: -moz-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #FF7070), color-stop(100%, #FF7070));
+    background: -webkit-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: -o-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: -ms-linear-gradient(top, #FF7070 0%, #FF7070 100%);
+    background: linear-gradient(to bottom, #fc2803 0%, #fc2803 100%);
+    background-size: 150% 150%;
+}
+
+@media (min-width: 992px){
+    .typo-line{
+        padding-left: 140px;
+        margin-bottom: 40px;
+        position: relative;
+    }
+
+    .typo-line .category{
+        transform: translateY(-50%);
+        top: 50%;
+        left: 0px;
+        position: absolute;
+    }
+}
+
+.all-icons [class*="pe-"]{
+    font-size: 40px;
+}
+.all-icons input{
+    border: 0;
+}
+.all-icons .font-icon-detail{
+    text-align: center;
+    padding: 45px 0px 30px;
+    border: 1px solid #e5e5e5;
+    border-radius: 6px;
+    margin: 15px 0;
+}
+.all-icons .font-icon-detail input{
+    margin: 25px auto 0;
+    width: 100%;
+    text-align: center;
+    display: block;
+    color: #aaa;
+    font-size: 13px;
+}
+
+#map{
+    position:relative;
+    width:100%;
+    height: calc(100% - 60px);
+}
+
+.places-buttons .btn{
+    margin-bottom: 30px
+}
+.sidebar .nav > li.active-pro{
+    position: absolute;
+    width: 100%;
+    bottom: 10px;
+}
+.sidebar .nav > li.active-pro a{
+    background: rgba(255, 255, 255, 0.14);
+    opacity: 1;
+    color: #FFFFFF;
+}
+
+.table-upgrade td:nth-child(2),
+.table-upgrade td:nth-child(3){
+    text-align: center;
+}
+
+@media all and (max-width: 991px)  {
+    .sidebar .nav > li.active-pro{
+        position: initial;
+    }
+}
+
+
 
 </style>
+
+<style src="@/assets/css/sb-admin-2.min.css">
