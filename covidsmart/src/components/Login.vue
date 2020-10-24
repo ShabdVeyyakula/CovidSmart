93783d5aeb0038dc65fd77aba6f4710b146b5538
HollandPleskac <hollandpleskac@gmail.com>
2020-10-24 00:58:13 -0700

    login

--- covidsmart/src/components/Login.vue
+++ covidsmart/src/components/Login.vue
@@ -1,20 +1,58 @@
 <template>
-    <h3 class="h">test</h3>
+  <div class="login">
+    <h1>Sign In</h1>
+    <form action="#" method="post" class="contact-form">
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Your email"
+        required
+      />
+      <input
+        type="text"
+        name="password"
+        id="password"
+        placeholder="Your password"
+        required
+      />
+      <input type="submit" value="Sign In" />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Login',
-  data () {
+  name: "Login",
+  data() {
     return {
-      msg: 'Welcome to the Sign In Page'
-    }
-  }
-}
+      msg: "Welcome to the Login Page",
+    };
+  },
+};
 </script>
 
 <style>
-    h3 {
-        color: white;
-    }
+
+h1 {
+
+}
+
+.login {
+  /* background-color: red; */
+  margin-top: 30vh;
+}
+
+.login input[type="text"],
+.login input[type="email"] {
+  margin: 10px auto;
+  padding: 7px;
+  width: 20%;
+  display: block;
+  border-radius: 4px;
+}
+
+*:focus {
+  outline: none;
+}
 </style>
