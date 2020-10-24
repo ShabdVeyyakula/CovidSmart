318263c30bada8c6869a641566a0e86bbbb1b7ac
HollandPleskac <hollandpleskac@gmail.com>
2020-10-23 23:59:15 -0700

    import components

--- covidsmart/src/router/index.js
+++ covidsmart/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import SignIn from '@/components/SignIn'
+import SignUp from '@/components/SignUp'
 
 Vue.use(Router)
 
