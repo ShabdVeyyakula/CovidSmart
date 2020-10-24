cb05b4c4e93e0538d0488083f34832a113ddcc8b
HollandPleskac <hollandpleskac@gmail.com>
2020-10-23 23:57:10 -0700

    routes

--- covidsmart/src/router/index.js
+++ covidsmart/src/router/index.js
@@ -10,6 +10,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: HelloIn
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
+    },
   ]
 })
